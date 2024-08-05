00862f6dc4604cf09a682b5090d24767ff23870c
Facundo <156367881+Facundo223@users.noreply.github.com>
2024-08-05 14:17:46 -0300

    Update baja.php

--- baja.php
+++ baja.php
@@ -1,15 +1,9 @@
 <?php
-$Nombre=$_REQUEST['Nombre'];
-$Apellido=$_REQUEST['Apellido'];
-$Telefono=$_REQUEST['Telefono'];
 $DNI=$_REQUEST['DNI'];
-$Cuota=$_REQUEST['cuota'];
-$FechadePago=$_REQUEST['FechadePago'];
 $conexion=mysqli_connect("localhost","root","")or die("problemas de conexion");
 mysqli_select_db($conexion,"base")or die("problemas en la seleccion de base de datos");
-$consulta="insert into alumno values('$Nombre','$Apellido','$Telefono','$DNI','$Cuota','$FechadePago')";
-mysqli_query($conexion,$consulta);
+$sql=("delete from tabla where dni='$DNI'");
+mysqli_query($conexion,$sql) or die ("problemas en select:".mysqli_error($conexion));
+echo "datos borrados correctamente";
 mysqli_close($conexion);
-echo"los datos se cargaron con exito";
-echo "<br>"
 ?>
