871fd28e9b9d1ae6f490104b688734b56212f631
Facundo <156367881+Facundo223@users.noreply.github.com>
2024-08-05 17:22:37 -0300

    Update baja.php

--- baja.php
+++ baja.php
@@ -1,8 +1,8 @@
 <?php
 $DNI=$_REQUEST['DNI'];
 $conexion=mysqli_connect("localhost","root","")or die("problemas de conexion");
-mysqli_select_db($conexion,"base")or die("problemas en la seleccion de base de datos");
-$sql=("delete from tabla where dni='$DNI'");
+mysqli_select_db($conexion,"gimnasio")or die("problemas en la seleccion de base de datos");
+$sql=("delete from clientes where DNI='$DNI'");
 mysqli_query($conexion,$sql) or die ("problemas en select:".mysqli_error($conexion));
 echo "datos borrados correctamente";
 mysqli_close($conexion);
