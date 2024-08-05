79f747e3651af32d2a8b871f873c008f47a8d067
Facundo <156367881+Facundo223@users.noreply.github.com>
2024-08-05 14:17:19 -0300

    Update alta.php

--- alta.php
+++ alta.php
@@ -1,9 +1,13 @@
 <?php
-$nombre=$_REQUEST['Nombre'];
-$apellido=$_REQUEST['Apellido'];
-$telefono=$_REQUEST['Telefono'];
-$conexion=mysqli_connect("localhost","root","","base");
-$consulta="insert into tabla values('$nombre','$apellido','$telefono')";
+$Nombre=$_REQUEST['Nombre'];
+$Apellido=$_REQUEST['Apellido'];
+$Telefono=$_REQUEST['Telefono'];
+$DNI=$_REQUEST['DNI'];
+$Cuota=$_REQUEST['cuota'];
+$FechadePago=$_REQUEST['FechadePago'];
+$conexion=mysqli_connect("localhost","root","")or die("problemas de conexion");
+mysqli_select_db($conexion,"base")or die("problemas en la seleccion de base de datos");
+$consulta="insert into alumno values('$Nombre','$Apellido','$Telefono','$DNI','$Cuota','$FechadePago')";
 mysqli_query($conexion,$consulta);
 mysqli_close($conexion);
 echo"los datos se cargaron con exito";
